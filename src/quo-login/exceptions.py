170e2f7ccbe646bcc5f2d74f6d6c7549fef8c93b
Gerrishon Sirere <s.gerrishon@gmail.com>
2021-03-28 11:07:06 +0300

    Create exceptions.py

---
+++ src/quo-login/exceptions.py
@@ -0,0 +1,8 @@
+from quo import HTTPException
+from starlette.status import HTTP_401_UNAUTHORIZED
+
+InvalidCredentialsException = HTTPException(
+    status_code=HTTP_401_UNAUTHORIZED,
+    detail="Invalid credentials",
+    headers={"WWW-Authenticate": "Bearer"}
+)
